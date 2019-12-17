c5c0befbda6423584dcfebda4502f3198366a64a
Ahmad Wilson <ahwilson712@gmail.com>
2019-12-17 13:05:32 -0500

    move system_dependencies and cl_dependencies to variable

---
+++ test/integration/install/archive_install/default_test.rb
@@ -0,0 +1,14 @@
+title "Lotus git installation test suite"
+
+describe directory('/opt/lotus') do
+  it { should exist }
+end
+
+describe file('/usr/local/bin/lotus') do
+  it { should exist }
+  its('mode') { should cmp '0775' }
+end
+
+describe command('/usr/local/bin/lotus --help') do
+  its('exit_status') { should eq 0 }
+end
